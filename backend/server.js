2fb667c0a1083566d3a636fa0687c2c9e4d3649a
TH01-B24DCCC109 <polamnam213@gmail.com>
2026-05-19 21:39:28 +0700

    update

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const cors = require("cors");
+
+require("dotenv").config();
+
+const orderRoute = require("./routes/orderRoute");
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.use("/api/orders", orderRoute);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(
+    `Server running on port ${PORT}`
+  );
+});
